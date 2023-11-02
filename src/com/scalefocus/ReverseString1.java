6bfb56b1213827a297e978260e0ddc01160d1a5d
eren.cengel <eren.cengel@scalefocus.com>
2023-11-02 16:50:10 +0300

    new task added

--- src/com/scalefocus/ReverseString1.java
+++ src/com/scalefocus/ReverseString1.java
@@ -5,6 +5,8 @@ public class ReverseString1 {
     public static void main(String[] args) {
         System.out.println(reverseString("eren"));
         System.out.println(reverseStringWithBasicActions("Alperen"));
+        System.out.println(re("aslıhan"));
+        System.out.println(re2("agaH"));
     }
 
     public static String reverseString(String str){
@@ -20,4 +22,15 @@ public class ReverseString1 {
         return reverseOne;
     }
 
+    static String re(String str){
+        return new StringBuffer(str).reverse().toString();
+    }
+
+    static String re2(String str){
+        String result = "";
+        for (int i = str.length()-1; i >=0 ; i--) {
+            result += str.charAt(i);
+        }
+        return result;
+    }
 }
---
+++ src/com/scalefocus/SubStrings.java
@@ -0,0 +1,71 @@
+package com.scalefocus;
+
+import java.util.*;
+
+public class SubStrings {
+
+    /*
+    Count of substrings having all distinct characters
+    Given a string str consisting of lowercase alphabets, the task is to find the number of possible substrings (not necessarily distinct) that consists of distinct characters only.
+    Examples:
+    Input: Str = “gffg”
+    Output: 6
+    Explanation:
+    All possible substrings from the given string are,
+    ( “g“, “gf“, “gff”, “gffg”, “f“, “ff”, “ffg”, “f“, “fg“, “g” )
+    Among them, the highlighted ones ( “g“, “gf“, “f“, “f“, “fg“, “g” ) consists of distinct characters only.
+    Input: str = “gfg”
+    Output: 5
+    Explanation:
+    All possible substrings from the given string are,
+    ( “g“, “gf“, “gfg”, “f“, “fg“, “g” )
+    Among them, the highlighted ( “g“, “gf“, “f“, “fg“, “g” ) consists of distinct characters only.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("returnDistinctAsList(addSubstringsToArraylist(\"eren\")) = " + returnDistinctAsList(addSubstringsToArraylist("eren")));
+        System.out.println("addSubstringsToArraylist2(\"eren\") = " + addSubstringsToArraylist2("eren"));
+    }
+
+    static List<String> addSubstringsToArraylist(String str){
+        List<String> result = new ArrayList<>();
+        int a = 0;
+        while (a < str.length()){
+            for (int j = a; j <= str.length(); j++){
+                result.add(str.substring(a,j));
+            }
+            a++;
+        }
+        result.removeIf(n -> n.equals(""));
+        return result;
+    }
+
+    static List<String> returnDistinctAsList(List<String> list){
+        List<String> distinctList = new ArrayList<>();
+        for (String s : list) {
+            boolean bool = true;
+            String[] split = s.split("");
+            for (String s1 : split) {
+                if(Collections.frequency(Arrays.asList(split),s1) != 1){
+                    bool = false;
+                    break;
+                }
+            }
+            if(bool) distinctList.add(s);
+        }
+        return distinctList;
+    }
+
+    static List<String> addSubstringsToArraylist2(String str){
+        List<String> result = new ArrayList<>();
+        int a = 0;
+        while (a < str.length()){
+            for (int j = a; j <= str.length(); j++){
+                result.add(str.substring(a,j));
+            }
+            a++;
+        }
+        result.removeIf(n -> n.equals(""));
+        return returnDistinctAsList(result);
+    }
+}
