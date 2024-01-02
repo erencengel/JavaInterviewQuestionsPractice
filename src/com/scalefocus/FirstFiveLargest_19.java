ae154d9b60d6bc24481f18572f01769cf959b0b3
eren.cengel <eren.cengel@scalefocus.com>
2024-01-02 14:14:40 +0300

    consumer

--- src/com/scalefocus/FirstFiveLargest_19.java
+++ src/com/scalefocus/FirstFiveLargest_19.java
@@ -3,6 +3,8 @@ package com.scalefocus;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class FirstFiveLargest_19 {
@@ -20,6 +22,21 @@ public class FirstFiveLargest_19 {
 
     public static void main(String[] args) {
         System.out.println(firstFiveLargest(new int[]{1, 5, 4, 0, 90, -5, 78}));
+        firstFive(new int[]{0,-10,5,500,1000,3,-90,-1,1001});
+    }
+
+    public static void firstFive(int[] number) {
+        Consumer<int[]> consumer = n -> {
+            Arrays.sort(n);
+            int[] arrayV2 = new int[n.length];
+            int j = 0;
+            for (int i = n.length - 1; i >= 0; i--) {
+                arrayV2[j] = n[i];
+                j++;
+            }
+            System.out.println(Arrays.toString(Arrays.stream(arrayV2).limit(5).toArray()));
+        };
+        consumer.accept(number);
     }
 
 }
